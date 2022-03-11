d12da9bbac48349feaa9eb0a03dcba50c078547e
muffydah <muffymohd2001@gmail.com>
2022-03-11 15:44:15 -0800

    Printed out a phrase and followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
 
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+/**
+ * main - prints the phrase and makes the code executable
+ * Return: 0 if exited properly, otherwise non-zero
+ */
+ int main(void)
+ {
+           puts("\"Programming is like buiding a multilingual puzzle");
+	   return (0);
+ } 
