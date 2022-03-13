bf48c1c5573e0d5c3b2db47ff257ffb28efb72fd
muffydah <muffymohd2001@gmail.com>
2022-03-12 23:14:54 -0800

    Printed out a text followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase and makes the code executable
- * Return: 0 if exited properly, otherwise non-zero
- */
- int main(void)
- {
-           puts("\"Programming is like buiding a multilingual puzzle");
-	   return (0);
- } 
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
