f2ba9d00c33deb06c8ae1a8f61b9caf2299af387
muffydah <muffymohd2001@gmail.com>
2022-03-14 08:58:30 -0700

    Assigned random number with conditions

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/*
+ * main - print if the number is positive, zero or negative
+ *
+ * Description: using the main function
+ * this program prints "The number is postive ,zero or negatic=ve"
+ * Return: 0 (sucess)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if (n > 0)
+	{	
+		printf("%d is positive\n", n);
+	}	
+	else if	(n == 0)
+	{
+		printf("%d is zero\n" ,n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0);
+
+}	
