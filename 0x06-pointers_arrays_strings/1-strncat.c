6fee6722b17a00b6b2ccc03c7a0cb6c9aaca21d9
muffydah <muffymohd2001@gmail.com>
2022-03-24 13:28:55 -0700

    Initial commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,47 +1,25 @@
 #include "main.h"
 
 /**
- * _strncat - concatenate n bytes to destination string
- * @dest: string to be appended to
- * @src: string to append
- * @n: append n number of bytes(chars)
- * Return: concatenated string
+ * _strncat - a function that concatenates two strings.
+ * @dest: string 1
+ * @src: string 2
+ * @n: amou
+ * Return: pointer to dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
-	int j = 0;
+	int i, j;
 
-	while (dest[i] != '\0') /*i is index of null terminator*/
-		i++;
-
-	while (src[j] != '\0' && j < n) /*append replacing null terminator*/
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
 		dest[i] = src[j];
 		i++;
-		j++;
 	}
 	dest[i] = '\0';
-
 	return (dest);
 }
-/* another method
-char *_strncat(char *dest, char *src, int n)
-{
-   //while dest exists and isn't null terminator yet
-   while (*dest)
-       dest++;
-   // replace dest while src exists and insert nulls till n bytes
-   for (i = 0; i < n; i++)
-   {
-	while (*src)
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-   }
-}
-*/
